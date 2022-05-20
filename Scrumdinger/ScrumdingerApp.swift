aeff843d8452efd3ae103bff33b1a8ed843b4d69
y.hoshi <ysk.hoshi.1118@gmail.com>
2022-05-20 20:34:12 +0900

    Initial Commit

---
+++ Scrumdinger/ScrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumdingerApp.swift
+//  Scrumdinger
+//
+//  Created by hossy on 2022/05/20.
+//
+
+import SwiftUI
+
+@main
+struct ScrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
