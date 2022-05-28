134146287269ce8fe4c11e66735cffa1da056f43
y.hoshi <ysk.hoshi.1118@gmail.com>
2022-05-28 15:37:32 +0900

    非アクティブ時にデータ保存できるように

--- Scrumdinger/DetailEditView.swift
+++ Scrumdinger/DetailEditView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct DetailEditView: View {
     @Binding var data: DailyScrum.Data
-    @State private var newAttendeesName = ""
+    @State private var newAttendeeName = ""
     
     var body: some View {
         Form {
@@ -19,7 +19,7 @@ struct DetailEditView: View {
                     Slider(value: $data.lengthInMinutes, in: 5...30, step: 1) {
                         Text("Length")
                     }
-                    .accessibilityLabel("\(Int(data.lengthInMinutes)) minutes")
+                    .accessibilityValue("\(Int(data.lengthInMinutes)) minutes")
                     Spacer()
                     Text("\(Int(data.lengthInMinutes)) minutes")
                         .accessibilityHidden(true)
@@ -27,25 +27,25 @@ struct DetailEditView: View {
                 ThemePicker(selection: $data.theme)
             }
             Section(header: Text("Attendees")) {
-                ForEach(data.attendees) { attendees in
-                    Text(attendees.name)
+                ForEach(data.attendees) { attendee in
+                    Text(attendee.name)
                 }
                 .onDelete { indices in
                     data.attendees.remove(atOffsets: indices)
                 }
                 HStack {
-                    TextField("New Attendee", text: $newAttendeesName)
+                    TextField("New Attendee", text: $newAttendeeName)
                     Button(action: {
                         withAnimation {
-                            let attendee = DailyScrum.Attendee(name: newAttendeesName)
+                            let attendee = DailyScrum.Attendee(name: newAttendeeName)
                             data.attendees.append(attendee)
-                            newAttendeesName = ""
+                            newAttendeeName = ""
                         }
                     }) {
                         Image(systemName: "plus.circle.fill")
                             .accessibilityLabel("Add attendee")
                     }
-                    disabled(newAttendeesName.isEmpty)
+                    .disabled(newAttendeeName.isEmpty)
                 }
             }
         }
